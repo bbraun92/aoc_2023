2213ddbf8164c962f50d2687b54ae623cade4368
Bastian Braun <bastian.braun@zaumstudio.com>
2023-12-03 00:33:44 +0100

    merge p1 and p2

--- 2/solution.go
+++ 2/solution.go
@@ -7,62 +7,21 @@ import (
 	"strings"
 )
 
-const FILE_SRC = "input.txt"
-const PREFIX = "Game "
-
 func main() {
-	fmt.Println("Solution to part one: ", partOne())
-	fmt.Println("Solution to part two: ", partTwo())
+	silverStar, goldStar := solve(12, 13, 14)
+	fmt.Println("Solution to part one: ", silverStar)
+	fmt.Println("Solution to part two: ", goldStar)
 }
 
-func partOne() int {
-	const MAX_RED = 12
-	const MAX_GREEN = 13
-	const MAX_BLUE = 14
-
-	lines := util.ReadFileLineByLine(FILE_SRC)
-	sumValid := 0
+func solve(maxRed, maxGreen, maxBlue int) (int, int) {
+	lines := util.ReadFileLineByLine("input.txt")
+	sumValid, sumPow := 0, 0
 
 	for _, line := range lines {
-		withoutPrefix, _ := strings.CutPrefix(line, PREFIX)
+		withoutPrefix, _ := strings.CutPrefix(line, "Game ")
 		gameString, roundsString, _ := strings.Cut(withoutPrefix, ":")
 
 		game, _ := strconv.Atoi(gameString)
-		rounds := strings.Split(roundsString, ";")
-
-		gameValid := true
-
-		for _, roundString := range rounds {
-			round := strings.Split(roundString, ",")
-			colorToCount := make(map[string]int)
-
-			for _, color := range round {
-				colorParts := strings.Split(color, " ")
-				count, _ := strconv.Atoi(colorParts[1])
-				colorToCount[colorParts[2]] = count
-			}
-
-			if colorToCount["red"] > MAX_RED || colorToCount["green"] > MAX_GREEN || colorToCount["blue"] > MAX_BLUE {
-				gameValid = false
-				break
-			}
-		}
-
-		if gameValid {
-			sumValid += game
-		}
-	}
-
-	return sumValid
-}
-
-func partTwo() int {
-	lines := util.ReadFileLineByLine(FILE_SRC)
-	sum := 0
-
-	for _, line := range lines {
-		withoutPrefix, _ := strings.CutPrefix(line, PREFIX)
-		_, roundsString, _ := strings.Cut(withoutPrefix, ":")
 		rounds := strings.Split(roundsString, ";")
 
 		colorToCount := map[string]int{
@@ -85,8 +44,12 @@ func partTwo() int {
 			}
 		}
 
-		sum += colorToCount["red"] * colorToCount["green"] * colorToCount["blue"]
+		if colorToCount["red"] <= maxRed && colorToCount["green"] <= maxGreen && colorToCount["blue"] <= maxBlue {
+			sumValid += game
+		}
+
+		sumPow += colorToCount["red"] * colorToCount["green"] * colorToCount["blue"]
 	}
 
-	return sum
+	return sumValid, sumPow
 }
